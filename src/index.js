5c7c239251be7de50b8d60330ad37972b8d28fdc
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-04-19 13:28:07 +0600

    fixing wanrings (2)

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ export default function Root() {
 
   useEffect(() => {
     setDarkMode(prefersDarkMode);
-    console.log(theme)
   }, [prefersDarkMode]);
 
   const handleDarkModeToggle = () => {
